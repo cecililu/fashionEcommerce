28efd76361b7398bf46fd8434f5f3782b1407ef5
cecil <cecilghimire@gmail.com>
2022-10-24 16:28:45 +0545

    added navbar

--- src/Components/common/Header.tsx
+++ src/Components/common/Header.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Head } from './Head'
 import { NavBar } from './NavBar'
-import { Search } from './Search'
+
 
 export const Header = () => {
   return (
    <div className=''>
    <Head/>
-   <Search/>
+   
    <NavBar/>
    </div>
   )
--- src/Components/common/NavBar.tsx
+++ src/Components/common/NavBar.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Search } from './Search'
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='grid grid-cols-3 gap-4 my-4 p-4 items-center'> 
+        <div className='logo '>
+          <span className='text-cyan-600 font-bold text-3xl'> CompanyLogo</span> 
+        </div>   
+        <div className='Search'>
+          <Search/>
+        </div>
+        <div className='add'>
+          ADD SPACE
+        </div>
+    </div>
   )
 }
