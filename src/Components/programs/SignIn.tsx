e51a6d70b8d16910ecbdd031dbf604be6d1b0c04
cecil <cecilghimire@gmail.com>
2022-11-01 19:53:45 +0545

    added formik and yup validation to the singin

--- src/Components/programs/SignIn.tsx
+++ src/Components/programs/SignIn.tsx
@@ -1,31 +1,52 @@
 import React from 'react'
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 export const SignIn = () => {
+    let schema = yup.object().shape({
+      email: yup.string().required(),
+      password: yup.string().required(),
+     })
+
+
+   const formik = useFormik({
+      initialValues: {
+      email: '',
+      password: ''},
+      
+      onSubmit: values => {
+        
+        alert(JSON.stringify(values, null, 2));
+      },
+      validationSchema:schema 
+     })
+ 
   return (
     <><section className="h-screen">
     <div className="px-6 h-full text-gray-800 flex justify-center mt-12">
      
         <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-          <form>
+          <form  onSubmit={formik.handleSubmit}>
               <p className="text-center  mx-4 text-2xl my-10">Sign In to Shop Now !</p>
-  
-            {/* <!-- Email input --> */}
+ 
             <div className="mb-6">
               <input
                 type="text"
                 className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                id="exampleFormControlInput2"
+                id="email"
                 placeholder="Email address"
+                onChange={formik.handleChange}
+                
               />
             </div>
-  
-            {/* <!-- Password input --> */}
             <div className="mb-6">
               <input
                 type="password"
                 className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                id="exampleFormControlInput2"
+                id="password"
                 placeholder="Password"
+                onChange={formik.handleChange}
+                
               />
             </div>
   
@@ -45,7 +66,7 @@ export const SignIn = () => {
   
             <div className="text-center lg:text-left">
               <button
-                type="button"
+                type="submit"
                 className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
               >
                 Login
