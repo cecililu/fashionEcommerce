8727f35b5387936f13ccc356149f7918a34a15ca
cecil <cecilghimire@gmail.com>
2022-11-02 16:44:37 +0545

    added Link to the Company logo HOME BUTTON link fixed

--- src/Components/common/NavBar.tsx
+++ src/Components/common/NavBar.tsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
     <div className='grid grid-cols-1 gap-1 my-4 p-2 items-center sm:grid-cols-4'> 
         <div className='logo flex justify-start  lg:justify-center mb-3 sm:mb-0 '>
         
-        <Link to="/signin">
+        <Link to="/">
         
           <span className='text-cyan-600 font-bold text-3xl ml-2 lg:justify-start '>
              Com Logo
