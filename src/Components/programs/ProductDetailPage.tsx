80436bfefc02130ffd9abb0c64de62cb7a506af9
cecil <cecilghimire@gmail.com>
2022-11-05 00:00:10 +0545

    css fix product display

--- src/Components/programs/ProductDetailPage.tsx
+++ src/Components/programs/ProductDetailPage.tsx
@@ -15,11 +15,11 @@ export const ProductDetailPage = () => {
         dispatch(fetchProductDetail()) 
        }, [])
   return (
-    <section className="text-gray-700 body-font overflow-hidden bg-white">
-    <div className="container px-5 py-24 mx-auto">
-      <div className="lg:w-4/5 mx-auto flex flex-wrap">
-        <img alt={productData.data['image']} className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={productData.data['image']}/>
-        <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+    <section className="h-screen">
+    <div className="px-6 h-full text-gray-800 flex justify-center mt-12">
+      <div className="sm:grid grid-cols-1 lg:grid-cols-2 gap-2">
+        <img alt={productData.data['image']} className="mx-auto w-1/2 object-fit rounded border border-gray-200 p-20" src={productData.data['image']}/>
+        <div className="py-20 px-20">
           <h2 className="text-sm title-font text-gray-500 tracking-widest">{productData.data['category']}</h2>
           <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{productData.data['title']}</h1>
           
@@ -27,24 +27,12 @@ export const ProductDetailPage = () => {
           <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
             <div className="flex">
             <div className="flex ml-6 items-center">
-              <span className="mr-3">Size</span>
-              <div className="relative">
-                {/* <select className="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
-                  <option>SM</option>
-                  <option>M</option>
-                  <option>L</option>
-                  <option>XL</option>
-                </select> */}
-                <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4" viewBox="0 0 24 24">
-                    <path d="M6 9l6 6 6-6"></path>
-                  </svg>
-                </span>
-              </div>
+              
+              
             </div>
           </div>
           <div className="flex">
-            <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
+            <span className="title-font font-medium text-2xl text-gray-900">{productData.data['price']}</span>
             <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Button</button>
             <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
               <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
