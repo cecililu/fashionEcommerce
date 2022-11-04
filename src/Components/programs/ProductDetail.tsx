1dc95619a45341625bba57d8aeb066ae83535110
cecil <cecilghimire@gmail.com>
2022-11-04 22:14:05 +0545

    added navigation to produt display

---
+++ src/Components/programs/ProductDetail.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AppState } from '../utils/types/types'
+
+export const ProductDetail = ({id}:any) => {
+  
+  
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/Components/utils/Navigation.tsx
+++ src/Components/utils/Navigation.tsx
@@ -3,6 +3,7 @@ import {
     BrowserRouter,
     Routes,
     Route,
+    useParams,
   } from "react-router-dom";
 import { CostumerCare } from '../programs/CostumerCare';
 import {CategoryNavigation} from '../programs/CategoryNavigation';
@@ -10,14 +11,18 @@ import { SignIn } from '../programs/SignIn';
 import { SignUp } from '../programs/SignUp';
 import { ProductDispay } from '../programs/ProductDispay';
 import { Home } from '../programs/Home';
+import { ProductDetail } from '../programs/ProductDetail';
   
 export const Navigation = (children:any) => {
+  let { userId } = useParams();
+  console.log('product no',userId)
   return (
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/customercare" element={<CostumerCare/>}/>
         <Route path="/signin" element={<SignIn/>}/>
         <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/product/:userId" element={<ProductDetail id={userId}/>}/>
     </Routes>
     )
 }
