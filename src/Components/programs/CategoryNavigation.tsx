f5b389f0ea66567b984fe621fcb747e884e66f25
cecil <cecilghimire@gmail.com>
2022-11-06 23:28:30 +0545

    css fix product sidebar

--- src/Components/programs/CategoryNavigation.tsx
+++ src/Components/programs/CategoryNavigation.tsx
@@ -4,11 +4,11 @@ import { Sidebar } from '../Sidebar/Sidebar'
 export const CategoryNavigation = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 sm:grid-cols-5">
+      <div className="grid grid-cols-1 sm:grid-cols-5 mt-5">
             <div className="col-span-0 sm:col-span-1 h-0">
              <Sidebar/>
               </div> 
-             <div className="main bg-cyan-200 col-span-4 ">
+             <div className="main bg-cyan-200 col-span-5 lg:col-span-4 ">
                <img className="" src="banner.jpg" alt="product image" />
              </div>
           </div>
