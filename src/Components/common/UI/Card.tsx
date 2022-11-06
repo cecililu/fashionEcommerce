52cfc2cb38d3b2efb4b168e4f713bf24b83c70a0
cecil <cecilghimire@gmail.com>
2022-11-06 22:59:22 +0545

    css fix product detail

--- src/Components/common/UI/Card.tsx
+++ src/Components/common/UI/Card.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Card = (data:any) => {
+    console.log(data)
     let id='product/'+data.data['id']
     return (
-    <div className="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-full  bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <Link to={id}>
     <a href="#">
-        <img className="object-fill h-48 w-50 mx-auto " src={data.data['image']} alt="product image" />
+        <img className="object-fill sm:h-20 lg:h-48 w-50 mx-auto " src={data.data['image']} alt="product image" />
     </a>
-    <div className="px-5 pb-5">
-        <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{data.data['title']}</h5>
-        </a>
+    <div className="pb-5">
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white flex-start">{data.data['title']}</h5>
+        
         <div className="flex items-center mt-2.5 mb-5">
             <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
             <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
--- src/Components/programs/ProductDetailPage.tsx
+++ src/Components/programs/ProductDetailPage.tsx
@@ -17,15 +17,15 @@ export const ProductDetailPage = () => {
         dispatch(fetchProductDetail({userId})) 
        }, [])
   return (
-    <section className="max-h-full pb-20 px-20 ml-20">
-    <div className="px-6  text-gray-800 flex justify-center mt-12 mx-auto border lg:p-25">
-      <div className="sm:grid grid-cols-1 lg:grid-cols-5 gap-2  mx-auto mt-10 border">
-        <img alt={productData.data['image']} className="lg:w-full mx-auto h-96 lg:h-full object-contain col-span-1 lg:col-span-1" src={productData.data['image']}/>
-        <div className="py-10 px-20 col-span-1 lg:col-span-3 mx-auto">
+    <section className="max-h-full pb-20  mx-auto">
+    <div className=" text-gray-800 flex justify-center lg:mt-12 mx-auto border lg:p-25">
+      <div className="mx-5 sm:grid grid-cols-1 lg:grid-cols-5 gap-2  mx-auto lg:mt-10 border">
+        <img alt={productData.data['image']} className="object-contain h-48 md: h-80 sm:col-span-1 mx-auto" src={productData.data['image']}/>
+        <div className="sm:py-10 sm:px-5 lg:py-10 lg:px-20 col-span-1 lg:col-span-3 mx-auto">
           <h2 className="text-sm title-font text-gray-500 tracking-widest mt-10">{productData.data['category']}</h2>
           <div className='grid-cols-1 '>
           <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{productData.data['title']}</h1> 
-          <p className="leading-relaxed text-justify px-5 mt-5">{productData.data['description']}.</p>
+          <p className="leading-relaxed text-justify  mt-5">{productData.data['description']}.</p>
           
           <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
           <div className="flex">
