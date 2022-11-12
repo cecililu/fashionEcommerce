0c1bc7b01cc44fc0c4bbbadb6b8e4f87064326c6
cecil <cecilghimire@gmail.com>
2022-11-12 12:14:38 +0545

    bug in cart

--- src/Components/common/UI/CartItem.tsx
+++ src/Components/common/UI/CartItem.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export const CartItem = (data:any) => {
+  const {title,price}=data.data.data
+  const{cartQuantity}= data.data
     return (
       <div> <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
       <div className="flex w-2/5"> 
@@ -8,22 +10,22 @@ export const CartItem = (data:any) => {
           <img className="h-24" src="https://drive.google.com/uc?id=1vXhvO9HoljNolvAXLwtw_qX3WNZ0m75v" alt=""/>
         </div>
         <div className="flex flex-col justify-between ml-4 flex-grow">
-          <span className="font-bold text-sm">Airpods</span>
-          <span className="text-red-500 text-xs">Apple</span>
+          <span className="font-bold text-sm">{title}</span>
+          <span className="text-red-500 text-xs">Brand name</span>
           <a href="#" className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
         </div>
       </div>
       <div className="flex justify-center w-1/5">
         <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
         </svg>
-        <input className="mx-2 border text-center w-8" type="text" value="1"/>
+        <input className="mx-2 border text-center w-8" type="text" value={cartQuantity}/>
   
         <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
           <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
         </svg>
       </div>
-      <span className="text-center w-1/5 font-semibold text-sm">$150.00</span>
-      <span className="text-center w-1/5 font-semibold text-sm">$150.00</span>
+      <span className="text-center w-1/5 font-semibold text-sm">${price}</span>
+      <span className="text-center w-1/5 font-semibold text-sm">${price* cartQuantity}</span>
     </div></div>
     )
   }
--- src/Components/utils/cartReducer/cartReducer.tsx
+++ src/Components/utils/cartReducer/cartReducer.tsx
@@ -10,17 +10,29 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
      addItem(state: any,action) {
-
-      const index=state.items.findIndex((item:any)=>item.id===action.payload)
+      
+      const index=state.items.findIndex((item:any)=>{
+              return item.data.id===action.payload.data.id
+            })
       //aready exist
       if (index>=0){ 
-        state.item[index].cartQuanity+=1
+        console.log(index,'already exists',state.items)
+        state.items[index].cartQuanity+=1
+        console.log('already exists',state.items[index].cartQuanity)
+    
+        
       }else{
       //not exist
+      console.log('new product added exists')
       const tempProduct={...action.payload,cartQuantity:1}
-      state.items.push(tempProduct);
-      }
+      state.items=[...state.items,tempProduct];
+
+      console.log(state.items[0])    
+      
+    }
       state.cartTotalQuantity=state.items.reduce((prevValue:any, currentValue:any) => prevValue + currentValue.cartQuantity,0)
+     
+
       
     }
   },
