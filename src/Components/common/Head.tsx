d34d6f1b4d5126456c4adfc2ca13374c4cc8ebdc
cecil <cecilghimire@gmail.com>
2022-12-07 10:59:48 +0545

    s

--- src/Components/common/Head.tsx
+++ src/Components/common/Head.tsx
@@ -11,19 +11,19 @@ export const Head = () => {
     <>
     <section className="head bg-cyan-500 text-white">
     <div className="flex flex-row lg:flex-row-reverse ">
-        <Link to="/customercare">
+        <Link to="/fashionEcommerce/customercare">
         <div className='mx-4'>Customer Care</div>
         </Link>
 
-        <Link to="/signin">
+        <Link to="/fashionEcommerce/signin">
         <div className='mx-4'>Sign In </div>
         </Link>
         
-        <Link to="/signup">
+        <Link to="/fashionEcommerce/signup">
         <div className='mx-4'>Sign Up</div>
         </Link>
 
-        <Link to="/cart">
+        <Link to="/fashionEcommerce/cart">
         <div className='mx-4'>Cart<span className='bg-red-700 px-2 mx-2 rounded-full' >{cart.cartTotalQuantity}</span></div>
         </Link>
       </div>
